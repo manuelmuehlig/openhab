3d655c8dbf02aad13f5105514c52735f70f2191c
Chris Jackson <chris@cd-jackson.com>
2015-05-01 16:32:22 +0100

    Update

--- bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/ZWaveActivator.java
+++ bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/ZWaveActivator.java
@@ -14,11 +14,15 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 
 import org.openhab.binding.zwave.internal.config.OpenHABConfigurationRecord;
+import org.openhab.binding.zwave.internal.config.ZWaveDbAssociationGroup;
+import org.openhab.binding.zwave.internal.config.ZWaveDbCommandClass;
 import org.openhab.binding.zwave.internal.config.ZWaveDbConfigurationListItem;
 import org.openhab.binding.zwave.internal.config.ZWaveDbConfigurationParameter;
 import org.openhab.binding.zwave.internal.config.ZWaveDbManufacturer;
 import org.openhab.binding.zwave.internal.config.ZWaveDbProduct;
 import org.openhab.binding.zwave.internal.config.ZWaveProductDatabase;
+import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveCommandClass;
+import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveCommandClass.CommandClass;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Version;
@@ -55,6 +59,7 @@ public final class ZWaveActivator implements BundleActivator {
 		
 		ZWaveProductDatabase database = new ZWaveProductDatabase();
 
+		int maxCfg = 0;
 		// Loop over the manufacturers
 		for (ZWaveDbManufacturer manufacturer : database.GetManufacturers()) {
 			if (database.FindManufacturer(manufacturer.Id) == false) {
@@ -69,92 +74,184 @@ public final class ZWaveActivator implements BundleActivator {
 
 			// Loop over the products
 			for (ZWaveDbProduct product : database.GetProducts()) {
+
+				int cfgCnt = 0;
 				
 				String outstring = "";
 				
 				
-				outstring += "<zwave:thing-descriptions bindingId=\"zwave\">";
+				outstring += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" ;
+				outstring += "<thing:thing-descriptions bindingId=\"zwave\"" ;
+				outstring += "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"" ;
+				outstring += "xmlns:thing=\"http://eclipse.org/smarthome/schemas/zwave-thing-description/v1.0.0\"" ;
+				outstring += "xsi:schemaLocation=\"http://eclipse.org/smarthome/schemas/zwave-thing-description/v1.0.0 http://eclipse.org/smarthome/schemas/zwave-thing-description/v1.0.0\">";
 
-				outstring += "<thing-type id=\"thingTypeID\">";
+				
+				
+				
+				String id = manufacturer.Name + "_" + product.Model + "_00_000"; 
+				outstring += "<thing-type id=\"" + id.replaceAll("\\s+","").toLowerCase() + "\">";
 				outstring += "<label>" + database.getLabel(product.Label) + "</label>";
-				outstring += "<description></description>";
+				outstring += "<description><![CDATA[ ]]></description>";
 
 				outstring += "<properties>";
-				outstring += "<property id=\"vendor\">" + manufacturer.Name + "</property>";
-				outstring += "<property id=\"model\">" + product.Model + "</property>";
-				outstring += "<property id=\"version\">" + "" + "</property>";
+				outstring += "<property name=\"vendor\">" + manufacturer.Name + "</property>";
+				outstring += "<property name=\"model\">" + product.Model + "</property>";
+				outstring += "<property name=\"version\">" + "" + "</property>";
 				outstring += "</properties>";
 
 				outstring += "<channels>";
 
 				outstring += "<channel id=\"switch0\" typeId=\"dimmer\">";
-				outstring += "<properties>";
+//				outstring += "<properties>";
 				outstring += "<property name=\"endpoint\">0</property>";
 				outstring += "<property name=\"command_class\">SWITCH_MULTILEVEL</property>";
 				outstring += "<property name=\"set_to_basic\">true</property>";
-				outstring += "</properties>";
+//				outstring += "</properties>";
 				outstring += "</channel>";
 
-				outstring += "</channels>";				
-				outstring += "</thing-type>";
+				outstring += "</channels>";	
 				
+				
+				database.FindProduct(manufacturer.Id, product.Reference.get(0).Type, product.Reference.get(0).Id, 255.99);
+				
+				// 
+				List<ZWaveDbConfigurationParameter> configList = database.getProductConfigParameters();
+				List<ZWaveDbAssociationGroup> groupList = database.getProductAssociationGroups();
+
+				if(configList != null || groupList != null) {
+					outstring += "<config-description>";
+
+					if(configList != null) {
+						outstring += "<parameter-group name=\"configuration\" context=\"setup\">";
+						outstring += "<label>Configuration Parameters</label>";
+						outstring += "<description></description>";
+						outstring += "</parameter-group>";
+					}
+					if(groupList != null) {
+						outstring += "<parameter-group name=\"association\" context=\"setup\">";
+						outstring += "<label>Association Groups</label>";
+						outstring += "<description></description>";
+						outstring += "</parameter-group>";
+					}
+
+					// Loop through the parameters and add to the records...
+					if(configList != null) {
+						for (ZWaveDbConfigurationParameter parameter : configList) {
+							cfgCnt++;
+							outstring += "<parameter name=\"config_" + parameter.Index+"\" type=\"integer\"";
+							if(parameter.Minimum != null) {
+								outstring += " min=\"" + parameter.Minimum + "\"";
+							}
+							if(parameter.Maximum != null) {
+								outstring += " max=\"" + parameter.Maximum + "\"";
+							}
+							if(parameter.ReadOnly != null) {
+								outstring += " readOnly=\"true\"";
+							}
+							outstring += ">";
+							
+							outstring += "<group>configuration</group>";
+							outstring += "<label>" + database.getLabel(parameter.Label) + "</label>";
+							outstring += "<description><![CDATA[" + database.getLabel(parameter.Help)+ "]]></description>";
+							
+							if(parameter.Default != null) {
+								outstring += "<default>" + parameter.Default + "</default>";
+							}
+		
+							if(parameter.Item != null) {
+								outstring += "<options>";
+	
+								for (ZWaveDbConfigurationListItem item : parameter.Item) {
+									outstring += "<option value=\"" + item.Value + "\">" + database.getLabel(item.Label) + "</option>";
+								}
+								outstring += "</options>";
+							}
+		
+							outstring += "</parameter>";
+						}
+					}
+	
+					if (groupList != null) {
+						// Loop through the associations and add to the
+						// records...
+						for (ZWaveDbAssociationGroup group : groupList) {
+							cfgCnt++;
+							outstring += "<parameter name=\"group_" + group.Index+"\" type=\"integer\"";
+							if(group.Maximum > 1) {
+								outstring += " multiple=\"" + group.Maximum + "\"";
+							}
+							outstring += ">";
+							outstring += "<group>association</group>";
+							outstring += "<label>" + database.getLabel(group.Label) + "</label>";
+							if(database.getLabel(group.Help) != null) {
+								outstring += "<description><![CDATA[" + database.getLabel(group.Help) + "]]></description>";
+							}
+							outstring += "</parameter>";
+						}
+					}
+					
+					outstring += "</config-description>";
+				}
+				
+				List<ZWaveDbCommandClass> classList = database.getProductCommandClasses();
+				if (classList != null) {
+					// First, loop through looking for the wakeup class
+					for (ZWaveDbCommandClass dbClass : classList) {
+						// needed or not?????
+					}
+
+					// Loop through the command classes
+					for (ZWaveDbCommandClass dbClass : classList) {
+						if(dbClass.add==null&&dbClass.isGetSupported==null&&dbClass.meterCanReset==null&&dbClass.remove==null&&dbClass.meterScale==null&&dbClass.meterType==null) {
+							continue;
+						}
+						outstring += "<property-group>";
+						outstring += "<property name=\"command_class\">" + CommandClass.getCommandClass(dbClass.Id) + "</property>";
+						if(dbClass.endpoint != null) {
+							outstring += "<property name=\"endpoint\">" + dbClass.endpoint + "</property>";
+						}
+						// If we want to remove the class, then remove it!
+						if(dbClass.remove != null && dbClass.remove == true) {
+							outstring += "<property name=\"command\">REMOVE</property>";
+						}
+						if(dbClass.add != null && dbClass.add == true) {
+							outstring += "<property name=\"command\">ADD</property>";
+						}
+
+						if(dbClass.isGetSupported != null && dbClass.isGetSupported == false) {
+							outstring += "<property name=\"get_supported\">false</property>";
+						}
+
+						if(dbClass.meterCanReset != null && dbClass.meterCanReset == true) {
+							outstring += "<property name=\"meter_canreset\">true</property>";
+						}
+						if(dbClass.meterType != null) {
+							outstring += "<property name=\"meter_canreset\">" + dbClass.meterType + "</property>";
+						}
+						if(dbClass.meterScale != null) {
+							outstring += "<property name=\"meter_canreset\">" + dbClass.meterScale + "</property>";
+						}
+						
+						outstring += "</property-group>";
+					}
+				}
+				
+				
+				outstring += "</thing-type>";
 				outstring += "<channel-type id=\"dimmer\">";
 				outstring += "<item-type>Dimmer</item-type>";
 				outstring += "<label>Dimmer</label>";
 				outstring += "<description>Set the light level</description>";
 				outstring += "<category>Light</category>";
 				outstring += "</channel-type>";
-				
-				database.FindProduct(manufacturer.Id, product.Reference.get(0).Type, product.Reference.get(0).Id, 255.99);
-				
-				boolean parms = false;
-				List<ZWaveDbConfigurationParameter> configList = database.getProductConfigParameters();
-				// Loop through the parameters and add to the records...
-				if(configList != null) {
-					for (ZWaveDbConfigurationParameter parameter : configList) {
-						if(parms == false) {
-							outstring += "<config-description>";
-							parms = true;
-						}
-						outstring += "<parameter name=\"" + parameter.Index+"\" type=\"integer\"";
-						if(parameter.Minimum != null) {
-							outstring += " min=\"" + parameter.Minimum + "\"";
-						}
-						if(parameter.Maximum != null) {
-							outstring += " max=\"" + parameter.Maximum + "\"";
-						}
-						if(parameter.ReadOnly != null) {
-							outstring += " readOnly=\"true\"";
-						}
-						outstring += ">";
-						
-						outstring += "<label>" + database.getLabel(parameter.Label) + "</label>";
-						outstring += "<description>" + database.getLabel(parameter.Help)+ "</description>";
-						
-						if(parameter.Default != null) {
-							outstring += "<default>" + parameter.Default + "</default>";
-						}
-	
-						if(parameter.Item != null) {
-							outstring += "<options>";
-
-							for (ZWaveDbConfigurationListItem item : parameter.Item) {
-								outstring += "<option value=\"" + item.Value + "\">" + database.getLabel(item.Label) + "</option>";
-							}
-							outstring += "<options>";
-						}
-	
-						outstring += "</parameter>";
-					}
-					if(parms == true) {
-						outstring += "</config-description>";
-					}
-				}
-				
 
 				
+				if(cfgCnt > maxCfg)
+					maxCfg = cfgCnt;
 				
-				outstring += "</zwave:thing-descriptions>";
+				
+				outstring += "</thing:thing-descriptions>";
 
 				if(outstring.isEmpty() == false) {
 					String name = manufacturer.Name.toLowerCase()+"_"+product.Model.toLowerCase() + ".xml";
@@ -173,6 +270,7 @@ public final class ZWaveActivator implements BundleActivator {
 			}
 		}
 
+		logger.debug("done Max cfg = {}", maxCfg);
 
 		
 		
